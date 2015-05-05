a13731fbbd7fe8678eab8526effb84dec56482e1
YOUR NAME <erinpagemd@yahoo.com>
2015-05-05 10:34:00 -0500

    setting up for tests

---
+++ test/units/test_birthday_parser.rb
@@ -0,0 +1,9 @@
+require_relative '../test_helper'
+
+class TestBirthdayParser < Minitest::Test
+
+  def test_the_test
+    assert false
+  end
+
+end
---
+++ test/units/test_cheer_generator.rb
@@ -0,0 +1,9 @@
+require_relative '../test_helper'
+
+class TestCheerGenerator < Minitest::Test
+
+  def test_the_test
+    assert false
+  end
+
+end
